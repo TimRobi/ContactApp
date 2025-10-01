e4f155369f4e30a0621aadb6eb42cb429e1b67d1
Timothy Robillard <timrobi@Mac.lan>
2025-10-01 15:43:51 -0400

    ContactApp

---
+++ frontend/src/App.jsx
@@ -0,0 +1,30 @@
+import { useState, useEffect } from 'react'
+import ContactList from './ContactList'
+import './App.css'
+import ContactForm from './ContactForm'
+
+function App() {
+  const [contacts, setContacts] = useState([{}])
+  const [isModalOpen, setIsModalOpen] = useState(false)
+
+  useEffect(() => {
+    fetchContacts()
+    }, [])
+
+  const fetchContacts = async () => {
+    const response = await fetch("http://127.0.0.1:5000/contacts")
+    const data = await response.json() 
+    setContacts(data.contacts)
+    console.log(data.contacts)
+  }
+
+
+  return (
+  <>
+    <ContactList contacts={contacts} />
+    <ContactForm />
+  </>
+  );
+}
+
+export default App
